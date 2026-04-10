915cc49d238fab6af75cead851294d31e9e1fd79
Emrys02 <emrys_02@outlook.com>
2026-04-10 10:47:26 +0100

    feat: expand countryflag arguments

--- lib/country_flag.dart
+++ lib/country_flag.dart
@@ -2,12 +2,33 @@ import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart' show SvgPicture;
 
 class CountryFlag extends StatelessWidget {
-  final String assetPath;
-  const CountryFlag({required this.assetPath, super.key});
+  final String _assetPath;
+  final double? _height;
+  final double? _width;
+  final BoxFit? _fit;
+  final AlignmentGeometry? _alignment;
+  const CountryFlag({
+    required String assetPath,
+    double? height,
+    double? width,
+    BoxFit? fit,
+    AlignmentGeometry? alignment,
+    super.key,
+  }) : _alignment = alignment,
+       _width = width,
+       _fit = fit,
+       _height = height,
+       _assetPath = assetPath;
 
   @override
   Widget build(BuildContext context) {
-    return SvgPicture.asset(assetPath, package: 'country_db');
-    ;
+    return SvgPicture.asset(
+      _assetPath,
+      package: 'country_db',
+      height: _height,
+      width: _width,
+      fit: _fit ?? .contain,
+      alignment: _alignment ?? .center,
+    );
   }
 }
